ec131a9ad8f23fe17caa235f061f872061201c06
Austin <akomen04@gmail.com>
2023-03-18 23:45:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, members) {
+    return drivers.filter(function (drivers){
+        return drivers.toLowerCase() ===members.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, members) {
+    return drivers.filter(function (drivers){
+        return drivers.toLowerCase().substring(0, members.length)===members.toLowerCase()
+    })
+}
+
+function matchName(drivers, hometown){
+    return drivers.filter(function (drivers) {
+        return drivers.name===hometown
+    })
+}
